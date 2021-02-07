91bf31824eec000b6acf784a94a3af32bfccc96a
Vladyslav Stusiak <vlstus377@gmail.com>
2021-02-07 13:37:01 +0100

    initial modules structure created

---
+++ data/src/main/java/org/stus/tracker/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the classes necessary to access data of domain
+ * <p>
+ *
+ * @since 0.0.1
+ */
+package org.stus.tracker;
---
+++ web/src/main/java/org/stus/tracker/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the classes necessary for web interface
+ * <p>
+ *
+ * @since 0.0.1
+ */
+package org.stus.tracker;
