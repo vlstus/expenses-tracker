6f3d110c054439d87089d0ce95f4f0a41aaf1023
Vladyslav Stusiak <vlstus377@gmail.com>
2021-02-07 14:53:58 +0100

    AbstractBaseEntity implemented as entity superclass User entity implemented

---
+++ data/src/main/java/org/stus/tracker/model/user/User.java
@@ -0,0 +1,31 @@
+package org.stus.tracker.model.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.stus.tracker.model.AbstractBaseEntity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter @Setter @RequiredArgsConstructor
+public class User extends AbstractBaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate registrationDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
